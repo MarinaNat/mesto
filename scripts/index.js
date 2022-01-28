9844c245d79ff8d759d8c12ff533964a1e940cbc
Marina Natalina <Kiska21mk@yandex.ru>
2022-01-28 14:36:04 +0300

    Проект 5 mesto исправление 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,30 @@
 const buttonProfile = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__close-btn ');
 const overlay = document.querySelector('.overlay');
+const overlayProfile = document.querySelector('.overlay_profile');
 const overlayCard = document.querySelector('.overlay_element');
 const overleyImage = document.querySelector('.overlay_image');
-let overlayActive = 'overlay_active';
-let profileInfo = document.querySelector('.profile__info');
-let buttonSave = document.querySelector('.popup__save-btn');
-let nameInput = document.querySelector('.popup__user_type_name');
-let jobInput = document.querySelector('.popup__user_type_status');
-let profileUserName = document.querySelector('.profile__user-name');
-let profileUserStatus = document.querySelector('.profile__user-status');
-let popup = document.querySelector('.popup');
-let cardAll = document.querySelector('.elements');
+const overlayActive = 'overlay_active';
+const profileInfo = document.querySelector('.profile__info');
+const buttonSave = document.querySelector('.popup__save-btn');
+const nameInput = document.querySelector('.popup__user_type_name');
+const jobInput = document.querySelector('.popup__user_type_status');
+const profileUserName = document.querySelector('.profile__user-name');
+const profileUserStatus = document.querySelector('.profile__user-status');
+const profilePopupForm = document.querySelector('.popup_profile');
+const cardAll = document.querySelector('.elements');
 const buttonCreate = document.querySelector('.profile__button-add');
-let cardNameInput = document.querySelector('.popup__element_type_name');
-let cardLinkInput = document.querySelector('.popup__element_type_link');
-let buttonCloseCard = document.querySelector('.popup__close-btn_element');
-let buttonSaveCard = document.querySelector('.popup__save-btn_element');
-let popupCard = document.querySelector('.popup_card');
+const cardNameInput = document.querySelector('.popup__element_type_name');
+const cardLinkInput = document.querySelector('.popup__element_type_link');
+const buttonCloseCard = document.querySelector('.popup__close-btn_element');
+const buttonSaveCard = document.querySelector('.popup__save-btn_element');
+const popupCard = document.querySelector('.popup_card');
 const picture = document.querySelector('.full-screen__image');
 const pictureTitle = document.querySelector('.full-screen__title');
 const buttonCloseFullImage = document.querySelector('.full-screen__close-btn');
 
 
-const initialCards = [{
+/*const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
@@ -47,7 +48,7 @@ const initialCards = [{
         name: 'Байкал',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-];
+];*/
 
 function openPopupCard(event) {
     event.preventDefault();
@@ -64,17 +65,12 @@ function like(event) {
     event.target.classList.toggle('element__like_active');
 }
 
-function openFullImage() {
-    overleyImage.classList.add(overlayActive);
-}
-
-function closeFullImage() {
-    overleyImage.classList.remove(overlayActive);
-}
-buttonCloseFullImage.addEventListener('click', closeFullImage);
+buttonCloseFullImage.addEventListener('click', () => {
+    closePopup(overleyImage)
+});
 
 //функция добавления карточек
-function fillingCards(name, link) {
+function fillingCard(name, link) {
     const cardTemplate = document.querySelector('#element-template').content.querySelector('.element');
     const cardElement = cardTemplate.cloneNode(true);
     const cardFoto = cardElement.querySelector('.element__foto');
@@ -90,13 +86,13 @@ function fillingCards(name, link) {
         pictureTitle.textContent = name;
         picture.src = link;
         picture.alt = name;
-        openFullImage()
+        openPopup(overleyImage);
     });
     return cardElement;
 };
 
 function renderCard(cardElement) {
-    cardAll.prepend(fillingCards(cardElement.name, cardElement.link));
+    cardAll.prepend(fillingCard(cardElement.name, cardElement.link));
 }
 //Перебор массива для создания карточек
 initialCards.forEach(function(initialCards) {
@@ -104,56 +100,62 @@ initialCards.forEach(function(initialCards) {
 });
 
 function closePopupCard() {
-    overlayCard.classList.remove(overlayActive);
     cardNameInput.value = '';
     cardLinkInput.value = '';
+    closePopup(overlayCard);
 }
 
 buttonCloseCard.addEventListener('click', closePopupCard)
     //создание карточек из попапа
-function NewfillingCards(e) {
+function newfillingCards(e) {
     e.preventDefault();
-    fillingCards(cardLinkInput.value, cardNameInput);
-    cardAll.prepend(fillingCards(cardNameInput.value, cardLinkInput.value))
+    fillingCard(cardLinkInput.value, cardNameInput);
+    cardAll.prepend(fillingCard(cardNameInput.value, cardLinkInput.value))
     closePopupCard()
 }
 
-popupCard.addEventListener('submit', NewfillingCards);
+popupCard.addEventListener('submit', newfillingCards);
 
-function openPopup(event) {
+function openPopupProfile(event) {
     event.preventDefault();
     nameInput.value = profileUserName.textContent;
     jobInput.value = profileUserStatus.textContent;
+    openPopup(overlayProfile);
+}
+
+buttonProfile.addEventListener('click', openPopupProfile);
+
+function openPopup(overlay) {
     overlay.classList.add(overlayActive);
 }
 
-buttonProfile.addEventListener('click', openPopup);
-
-function closePopup() {
+function closePopup(overlay) {
     overlay.classList.remove(overlayActive);
 }
 
-buttonClose.addEventListener('click', closePopup);
+buttonClose.addEventListener('click', () => {
+    closePopup(overlayProfile)
+});
 
 function clickOverlay(event) {
     if (event.target === overlay) {
-        closePopup();
+        closePopup(overlayProfile);
     }
 }
 overlay.addEventListener('click', clickOverlay);
 
 function clickEsc(event) {
     if (event.code === 'Escape') {
-        closePopup();
+        closePopup(overlayProfile);
     }
 }
 
 document.addEventListener('keydown', clickEsc);
 
-function formSubmitHandler(event) {
+function submiteProfileForm(event) {
     event.preventDefault();
     profileUserName.textContent = nameInput.value;
     profileUserStatus.textContent = jobInput.value;
-    closePopup()
+    closePopup(overlayProfile)
 };
-popup.addEventListener('submit', formSubmitHandler);
+profilePopupForm.addEventListener('submit', submiteProfileForm);
